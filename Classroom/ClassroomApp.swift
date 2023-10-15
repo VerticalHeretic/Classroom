dbbecc1696a36d449e58fbf6416f83f66ae6f1fc
Łukasz Stachnik <ls.warss98@gmail.com>
2023-10-15 20:49:49 +0200

    Initial Commit

---
+++ Classroom/ClassroomApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassroomApp.swift
+//  Classroom
+//
+//  Created by Łukasz Stachnik on 15/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ClassroomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
